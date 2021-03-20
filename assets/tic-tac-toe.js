84667c92296992e0dc73654e8176151e2af54288
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-03-20 23:48:40 +0600

    me

--- assets/tic-tac-toe.js
+++ assets/tic-tac-toe.js
@@ -142,6 +142,8 @@
                     } else if (empty.includes(0)) {
                         btns[0].click();
                     }
+                } else if (a2.length === 1 && !empty.includes(0)) {
+                    btns[2].click()
                 } else if (aaa == false && empty !== undefined) {
                     btns[empty[Math.floor(Math.random() * empty.length)]].click();
                 }
