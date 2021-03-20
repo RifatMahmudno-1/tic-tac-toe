4f7aff6b48ba21e88dee2017f556eaa44ed9a425
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-03-20 22:10:35 +0600

    me

--- assets/tic-tac-toe.js
+++ assets/tic-tac-toe.js
@@ -15,11 +15,11 @@
     function BtnCon() {
         if (wndw.w >= wndw.h) {
             document.querySelector('.btn-container').style = `width: ${wndw.h*0.7}px; height: ${wndw.h*0.7}px`
-            wndw.w / 40 > 3 ? aa = 30 : aa = wndw.w / 40
+            wndw.w / 40 > 30 ? aa = 30 : aa = wndw.w / 40
             document.querySelector('body').style = `font-size: ${aa}px;`
         } else {
             document.querySelector('.btn-container').style = `width: ${wndw.w*0.7}px; height: ${wndw.w*0.7}px`
-            wndw.h / 40 > 3 ? aa = 30 : aa = wndw.h / 40
+            wndw.h / 40 > 30 ? aa = 30 : aa = wndw.h / 40
             document.querySelector('body').style = `font-size: ${aa}px;`
         }
         document.querySelector('.win .main').style = `display: grid; grid-template-columns:${gtc}px ${document.querySelector('.win .main p').offsetWidth}px; grid-gap: 5px`
